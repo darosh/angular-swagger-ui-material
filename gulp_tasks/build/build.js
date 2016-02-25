40f54200cc87b8c7d1e0bb8343923b00124c1eec
Jan Forst <ensonador@gmail.com>
2016-02-25 15:14:22 +0100

    adding eslint to build task

--- gulp_tasks/build/build.js
+++ gulp_tasks/build/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['scripts', 'full', 'markdown', 'sort', 'styles']);
+gulp.task('build', ['scripts', 'full', 'markdown', 'sort', 'styles', 'eslint']);
