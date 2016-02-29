254f62ffc3402cd6660f1c9417f537f09427aab3
Jan Forst <ensonador@gmail.com>
2016-02-29 13:28:19 +0100

    (fix) missing metods + logical sort

--- src/modules/swagger-sort.js
+++ src/modules/swagger-sort.js
@@ -2,13 +2,23 @@
 
 angular.module('swaggerUiSort', ['swaggerUi'])
     .service('sortResult', function ($q) {
+        var order = {
+            get: 1,
+            head: 2,
+            options: 3,
+            post: 4,
+            put: 5,
+            patch: 6,
+            delete: 7,
+        }
+
         this.execute = function (parseResult) {
             var deferred = $q.defer();
 
             angular.forEach(parseResult.resources, function (resource) {
                 resource.operations.sort(function (a, b) {
-                    return (a.path.toLowerCase().replace(/[^a-z]+/gi, '') + '-' + a.httpMethod)
-                        .localeCompare(b.path.toLowerCase().replace(/[^a-z]+/gi, '') + '-' + b.httpMethod);
+                    return (a.path.toLowerCase().replace(/[^a-z]+/gi, '') + '-' + (order[a.httpMethod] || 9))
+                        .localeCompare(b.path.toLowerCase().replace(/[^a-z]+/gi, '') + '-' + (order[b.httpMethod] || 9));
                 });
             });
 
