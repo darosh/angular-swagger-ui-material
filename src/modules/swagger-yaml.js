efe30357af92a904ce5310e339daf1914bb8bcf1
Jan Forst <ensonador@gmail.com>
2016-02-29 07:44:10 +0100

    (fix) YAML parser

--- src/modules/swagger-yaml.js
+++ src/modules/swagger-yaml.js
@@ -1,12 +1,32 @@
 'use strict';
 
 angular.module('swaggerUiYaml', ['swaggerUi'])
+    .service('loadYaml', function ($q, $window) {
+        this.execute = function (options) {
+            var deferred = $q.defer();
+
+            options.transformResponse = function (data, headersGetter) {
+                if (headersGetter('content-type').indexOf('text/yaml') > -1) {
+                    return $window.jsyaml.load(data);
+                } else {
+                    return JSON.parse(data);
+                }
+            };
+
+            deferred.resolve(true);
+
+            return deferred.promise;
+        };
+    })
+    .run(function (swaggerModules, loadYaml) {
+        swaggerModules.add(swaggerModules.BEFORE_LOAD, loadYaml);
+    })
     .service('parseYaml', function ($q, $window, swaggerParser) {
         this.execute = function (parserType, url, contentType, data, isTrustedSources, parseResult) {
             var deferred = $q.defer();
 
             if (contentType === 'text/yaml') {
-                swaggerParser.execute('json', url, contentType, $window.jsyaml.load(data), isTrustedSources, parseResult);
+                swaggerParser.execute('json', url, contentType, data, isTrustedSources, parseResult);
                 deferred.resolve(true);
             } else {
                 deferred.resolve(false);
