bfdaa8221a270f0a0045781e1b17b5e3c6de196e
Jan Forst <ensonador@gmail.com>
2016-03-05 01:39:43 +0100

    (hub) :rainbow:

--- gulp_tasks/demo/copy.js
+++ gulp_tasks/demo/copy.js
@@ -3,7 +3,7 @@ var gulp = require('gulp');
 gulp.task('copy', ['build'], function () {
     return gulp.src(['dist/scripts/swagger-ui-material.full.min.js',
         'dist/styles/swagger-ui-material.min.css',
-        'src/hub.html',
+        'src/hub.*',
         'test/fixtures/examples/*.json',
         'test/fixtures/markdown/*.json']).pipe(gulp.dest('demo'));
 });
