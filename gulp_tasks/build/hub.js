a507f594bfb931bc6b4298c2ca0386a2d6bd9df4
Jan Forst <ensonador@gmail.com>
2016-03-05 22:04:15 +0100

    (hub) adding filters and search

--- gulp_tasks/build/hub.js
+++ gulp_tasks/build/hub.js
@@ -4,10 +4,26 @@ gulp.task('hub', function (done) {
     var fs = require('fs');
     var path = require('path');
     var rainbows = require('../../lib/api-models-rainbows');
+    var meta = require('../../lib/api-models-meta');
 
-    rainbows.load(function (data) {
-        fs.writeFile(path.join(__dirname, '../../src/hub.json'), JSON.stringify(data), function () {
-            done();
+    rainbows.load(function (rs) {
+        meta.load(function (ms) {
+            var data = {};
+            var keys = Object.keys(ms);
+
+            for (var i = 0; i < keys.length; i++) {
+                var k = keys[i];
+
+                data[k] = {
+                    colors: rs[k],
+                    security: ms[k].security,
+                    cors: ms[k].cors
+                };
+            }
+
+            fs.writeFile(path.join(__dirname, '../../src/hub.json'), JSON.stringify(data), function () {
+                done();
+            });
         });
     });
 });
