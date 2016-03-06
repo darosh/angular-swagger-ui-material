13e299ae9e8278cd770740db7601e4dc2eb5f66d
Jan Forst <ensonador@gmail.com>
2016-03-06 22:23:28 +0100

    (ref) making dialog service more reusable

--- src/services/dialog.js
+++ src/services/dialog.js
@@ -6,9 +6,9 @@ angular.module('swaggerUiMaterial')
             show: show
         };
 
-        function show ($event, locals) {
+        function show ($event, locals, type) {
             $mdDialog.show({
-                templateUrl: 'views/dialog.html',
+                templateUrl: 'views/' + (type || 'dialog') + '.html',
                 clickOutsideToClose: true,
                 targetEvent: $event,
                 controller: DialogCtrl,
@@ -18,7 +18,9 @@ angular.module('swaggerUiMaterial')
 
         function DialogCtrl ($scope, $mdDialog, vm) {
             $scope.vm = vm;
+            $scope.vm.opened = true;
             $scope.closeDialog = function () {
+                $scope.vm.opened = false;
                 $mdDialog.hide();
             };
         }
