604c0e7e236d8ec3b6d3fa78058e1f9b9360289d
Jan Forst <ensonador@gmail.com>
2016-03-10 09:55:42 +0100

    (hub) redesign

--- gulp_tasks/build/hub.js
+++ gulp_tasks/build/hub.js
@@ -5,6 +5,7 @@ gulp.task('hub', function (done) {
     var path = require('path');
     var rainbows = require('../../lib/api-models-rainbows');
     var meta = require('../../lib/api-models-meta');
+    var manual = require('../../test/manual/manual.json');
 
     rainbows.load(function (rs) {
         meta.load(function (ms) {
@@ -17,8 +18,21 @@ gulp.task('hub', function (done) {
                 data[k] = {
                     colors: rs[k],
                     security: ms[k].security,
-                    cors: ms[k].cors
+                    cors: ms[k].cors,
+                    operations: ms[k].operations
                 };
+
+                if (manual[k] && (manual[k].tested !== undefined)) {
+                    data[k].tested = manual[k].tested;
+                }
+
+                if (manual[k] && manual[k].local) {
+                    data[k].security.push('local');
+                }
+
+                // if (manual[k].notes !== undefined) {
+                // data[k].notes = manual[k].notes;
+                // }
             }
 
             fs.writeFile(path.join(__dirname, '../../src/hub.json'), JSON.stringify(data), function () {
